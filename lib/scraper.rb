c58b9c77dc2ee66693bda15117b3c778bb2e470e
Jonathan Wong <jonathan.wong110@gmail.com>
2019-03-18 13:49:15 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,14 +1,17 @@
 require 'nokogiri'
 require 'open-uri'
-
+require 'pry'
+ 
 require_relative './course.rb'
-
+ 
 class Scraper
  
   def get_page
-      # more code coming soon!
+      doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
+ 
+      binding.pry
   end
  
 end
-
-
+ 
+Scraper.new.get_page
